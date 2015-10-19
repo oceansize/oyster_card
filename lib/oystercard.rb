69a289687902090c13d0d165d072f1a5220294a8
Roi Driscoll <roi.driscoll@gmail.com>
2015-10-19 12:44:02 +0100

    Touch in and out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,38 @@
 class Oystercard
 
+  LIMIT = 90
+
   attr_reader :balance
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
+
+  def top_up(amount_received)
+    self.balance += amount_received
+    raise "Over limit of £#{LIMIT}" if balance > LIMIT
+  end
+
+  def deduct(amount_deducted)
+    self.balance -= amount_deducted
+  end
+
+  def in_journey?
+    in_journey
+  end
+
+  def touch_in
+    self.in_journey = true
+  end
+
+  def touch_out
+    self.in_journey = false
+  end
+
+  private
+
+  attr_writer :balance
+  attr_accessor :in_journey
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,9 +2,42 @@ require 'oystercard'
 
 describe Oystercard do
 
+  subject(:oystercard) { described_class.new }
+
   it "has a balance" do
-    oystercard = Oystercard.new
     expect(oystercard.balance).to eq(0)
   end
+
+  it "can be topped up with an amount of money" do
+    oystercard.top_up(10)
+    expect(oystercard.balance).to eq(10)
+  end
+
+  it "prevents more than the limit on the oystercard being topped up" do
+    large_amount = described_class::LIMIT + 10
+    expect { oystercard.top_up(large_amount) }.to raise_error "Over limit of £90"
+  end
+
+  it "by default does not have an active journey" do
+    expect(oystercard).to_not be_in_journey
+  end
+
+  it "is in a journey after touching in" do
+    oystercard.touch_in
+    expect(oystercard).to be_in_journey
+  end
+
+  it "is not in a journey when the user has touched out" do
+    oystercard.touch_in
+    oystercard.touch_out
+    expect(oystercard).to_not be_in_journey
+  end
+
+  it "deducts money from the card" do
+    oystercard.top_up(20)
+    oystercard.deduct(10)
+    expect(oystercard.balance).to eq(10)
+  end
+
 
 end
