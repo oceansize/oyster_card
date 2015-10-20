a241d38b02166129ebcd0027903656509120400b
Roi Driscoll <roi.driscoll@gmail.com>
2015-10-20 12:14:45 +0100

    Touching out adds station to journey history

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,38 +1,54 @@
 class Oystercard
 
-  LIMIT = 90
+  MAXIMUM_BALANCE = 90
+  MINIMUM_BALANCE = 1
 
-  attr_reader :balance
+  attr_reader :balance, :entry_station, :journey_history
 
   def initialize
     @balance = 0
-    @in_journey = false
+    @journey_history = []
   end
 
   def top_up(amount_received)
     self.balance += amount_received
-    raise "Over limit of £#{LIMIT}" if balance > LIMIT
+    raise "Over limit of £#{MAXIMUM_BALANCE}" if above_maximum_balance?
+  end
+
+  def in_journey?
+    !entry_station.nil?
+  end
+
+  def touch_in(station)
+    raise "Too low funds" if below_minimum_balance?
+    self.entry_station = station
+  end
+
+  def touch_out
+    self.journey_history << entry_station
+    self.entry_station = nil
+    deduct(journey_fare)
+  end
+
+  private
+
+  attr_writer :balance, :entry_station
+  attr_accessor :in_journey
+
+  def journey_fare
+    1
   end
 
   def deduct(amount_deducted)
     self.balance -= amount_deducted
   end
 
-  def in_journey?
-    in_journey
+  def below_minimum_balance?
+    balance < MINIMUM_BALANCE
   end
 
-  def touch_in
-    self.in_journey = true
+  def above_maximum_balance?
+    balance > MAXIMUM_BALANCE
   end
-
-  def touch_out
-    self.in_journey = false
-  end
-
-  private
-
-  attr_writer :balance
-  attr_accessor :in_journey
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,40 +4,74 @@ describe Oystercard do
 
   subject(:oystercard) { described_class.new }
 
-  it "has a balance" do
+  it "has a balance of £0" do
     expect(oystercard.balance).to eq(0)
   end
 
-  it "can be topped up with an amount of money" do
-    oystercard.top_up(10)
-    expect(oystercard.balance).to eq(10)
-  end
-
-  it "prevents more than the limit on the oystercard being topped up" do
-    large_amount = described_class::LIMIT + 10
-    expect { oystercard.top_up(large_amount) }.to raise_error "Over limit of £90"
-  end
-
-  it "by default does not have an active journey" do
+  it "by default is not in an active journey" do
     expect(oystercard).to_not be_in_journey
   end
 
-  it "is in a journey after touching in" do
-    oystercard.touch_in
-    expect(oystercard).to be_in_journey
+  it "has an empty journey list by default" do
+    expect(oystercard.journey_history).to eq([])
   end
 
-  it "is not in a journey when the user has touched out" do
-    oystercard.touch_in
-    oystercard.touch_out
-    expect(oystercard).to_not be_in_journey
+  describe "Topping up" do
+
+    it "can be topped up with an amount of money" do
+      oystercard.top_up(10)
+      expect(oystercard.balance).to eq(10)
+    end
+
+    it "prevents more than the limit on the oystercard being topped up" do
+      large_amount = described_class::MAXIMUM_BALANCE + 10
+      expect { oystercard.top_up(large_amount) }.to raise_error "Over limit of £90"
+    end
   end
 
-  it "deducts money from the card" do
-    oystercard.top_up(20)
-    oystercard.deduct(10)
-    expect(oystercard.balance).to eq(10)
+  describe "Touching in and out" do
+
+    let(:station) { :station }
+
+    context "with adequate funds for travel" do
+
+      before do
+        oystercard.top_up(1)
+      end
+
+      it "remembers the entry station after touching in" do
+        oystercard.touch_in(station)
+        expect(oystercard.entry_station).to eq(:station)
+      end
+
+      it "is in an active journey after touching in" do
+        oystercard.touch_in(station)
+        expect(oystercard).to be_in_journey
+      end
+
+      it "is not in an active journey when the user has touched out" do
+        oystercard.touch_in(station)
+        oystercard.touch_out
+        expect(oystercard).to_not be_in_journey
+      end
+
+      it "deducts the journey fare once the user touches out" do
+        oystercard.touch_in(station)
+        expect{ oystercard.touch_out }.to change{ oystercard.balance }.by(-1)
+      end
+
+      it "stores a journey after you've touched in and out" do
+        oystercard.touch_in(station)
+        oystercard.touch_out
+        expect(oystercard.journey_history).to eq([station])
+      end
+    end
+
+    context "without adequate funds for travel" do
+      it "does not allow you to touch in when below the minimum fare" do
+        expect { oystercard.touch_in(station) }.to raise_error "Too low funds"
+      end
+    end
+
   end
-
-
 end
